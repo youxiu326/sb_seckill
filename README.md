3dc3800afb875f5330f754a14923971d77969d49
youxiu3266 <2473538988@qq.com>
2019-11-07 13:18:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## 测试入口
+http://localhost:8080/swagger-ui.html
+
---
+++ src/main/java/com/youxiu326/SbSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.youxiu326;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbSeckillApplication.class, args);
+    }
+
+}
